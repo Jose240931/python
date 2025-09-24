59570a90bfb7653438972eaa92055dd15f26991b
Jose Garrido <a24galujo@iesgrancapita.org>
2025-09-24 19:01:53 +0200

    commit

---
+++ Colecciones.py
@@ -0,0 +1,22 @@
+#Lista
+numeros=[1,2,3,4,5,6,7,8,9,10]
+listaMixta=[10,"naranja",True]
+
+print (numeros[1])
+print (listaMixta[1])
+
+#tuplas
+contacto = ("Pepe manuel", 634132987)
+print (contacto[1])
+
+#Set
+numero={1,1,2,3,4,5,1,6,7,2,8,9}
+print(numero)
+
+#Diccionario
+contactos={"nombre":"paco","telefono":348358281}
+print(contactos["telefono"])
+print (contactos.keys())
+
+#String
+string="esto es una cadena de texto"
