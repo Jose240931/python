09c74f03e6f447dcea69fe2e96601d97d7734628
antsehe <a18sehean@iesgrancapitan.org>
2025-09-24 17:50:34 +0200

    commit

--- Bucles.py
+++ Bucles.py
@@ -1,9 +1,11 @@
 #Bucle for
+print("Bucle for")
 for i in range(1,10):
-    print(f"Bucle{i}")
+    print(f"Bucle {i}")
 
 #Bucle while
 j=0
+print("Bucle while")
 while j<10:
-    print(f"Bucle{j}")
+    print(f"Bucle {j}")
     j+=1
