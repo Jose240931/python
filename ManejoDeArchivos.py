7e12f5c4be0698eaca7526ee0a5d511d473d5d8b
antsehe <a18sehean@iesgrancapitan.org>
2025-09-24 18:53:10 +0200

    commit

---
+++ ManejoDeArchivos.py
@@ -0,0 +1,11 @@
+#Para abrir un archivo utilizaremos open()
+archivo=open("./Prueba/txt.txt",'r+')
+#Para escribir en un archivo de texto usaremos
+#el metodo write()
+archivo.write('Segunda linea \n')
+#Para escribir un numero, lo más común es convertir el número a cadena
+numero=5
+archivo.write(str(numero))
+archivo.seek(0)
+print(archivo.read())
+archivo.close()
