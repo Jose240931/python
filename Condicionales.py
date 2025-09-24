9dc8d2e0f048f378c538136549f226105481d438
Jose Garrido <a24galujo@iesgrancapita.org>
2025-09-24 17:46:38 +0200

    commit

--- Condicionales.py
+++ Condicionales.py
@@ -1,6 +1,7 @@
 #Condicional if/else
 edad=15
 trabaja=False
+numero=8
 if edad>=18:
     print("Eres mayor de edad")
 else:
@@ -17,3 +18,26 @@ else:
 #Condicional multiple
 if edad>18 and trabaja==True:
     print ("Eres un adulto")
+
+#Condicional match-case
+match numero:
+    case 1:
+        print("Seras medico y tendras ",edad," años")
+    case 2:
+        print ("Seras piloto y tendras  ",edad," años")
+    case 3:
+        print ("Seras profesor y tendras ",edad," años")
+    case 4:
+        print ("Seras programador y tendras ",edad," años")
+    case 5:
+        print ("Seras bombero y tendras ",edad," años")
+    case 6:
+        print ("Seras policia y tendras ",edad," años")
+    case 7:
+        print ("Seras ingeniero y tendras ",edad," años")
+    case 8:
+        print("Seras mecanico y tendras ",edad," años")
+    case 9:
+        print("Seras diseñador 3d y tnedras ",edad," años")
+    case 10:
+        print("Seras militar y tendras ",edad," años")
