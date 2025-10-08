6126d082feed08d5c18d50a257b728df21cb4c82
antsehe04 <antsehe04@gmail.com>
2025-10-08 17:56:34 +0200

    commit

--- ManejoDeArchivos.py
+++ ManejoDeArchivos.py
@@ -2,7 +2,7 @@
 archivo=open("./Prueba/txt.txt",'r+')
 #Para escribir en un archivo de texto usaremos
 #el metodo write()
-archivo.write('Segunda linea \n')
+archivo.write('Primera linea \n')
 #Para escribir un numero, lo más común es convertir el número a cadena
 numero=5
 archivo.write(str(numero))
